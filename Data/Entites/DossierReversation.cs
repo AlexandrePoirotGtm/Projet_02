059b6168c57781fd342ad05b91c26d6fc4749265
cristtiano <cristtianopinto@gmail.com>
2018-08-31 12:06:52 +0200

    1 - Creation des constructs pour la classe DossierReservation. 2 - Changement des champs et add de ForeignKey. 3 - Add Méthode ModifierClient et EffacerClient par OBJ client. 4 - Add Méthode ModifierDestination et EffacerDestination par OBJ Destination.

--- Data/Entites/DossierReversation.cs
+++ Data/Entites/DossierReversation.cs
@@ -8,7 +8,27 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Data
 {
     public class DossierReservation
-    {       
+    {
+        public DossierReservation()
+        {
+        }
+        public DossierReservation(string ncb, decimal prix, Voyage v, Client c)
+        {
+            NumeroCarteBancaire = ncb;
+            PrixParPersonne = prix;
+            Voyage = v;
+            Client = c;
+        }
+        public DossierReservation(string ncb, decimal prix,Voyage v,Client c, List<Participant> p_s)
+        {
+            NumeroCarteBancaire = ncb;
+            PrixParPersonne = prix;
+            Voyage = v;
+            Client  = c;
+            Participants = p_s;
+        }
+        
+
         public int Id { get; set; }        
         public string NumeroCarteBancaire { get; set; }
         public decimal PrixParPersonne { get; set; }
@@ -20,7 +40,7 @@ namespace Data
         public Client Client { get; set; }
 
         [NotMapped]
-        public Participant[] Participants { get; set; }
+        public List<Participant> Participants { get; set; }
        
 
         public List<Assurance> Assurances { get; set; }
